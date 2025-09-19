dbe748e056444d4211d0207cfbf2c66555d64e98
desikaaa <desikarmila211@gmail.com>
2025-09-19 12:16:47 +0700

    constanta dan final

--- project1/bin/project1.dart
+++ project1/bin/project1.dart
@@ -1,5 +1,5 @@
 import 'package:project1/project1.dart' as project1;
-
+const String nama = 'Desi';
 void main(List<String> arguments) {
   // print('Hello world: ${project1.calculate()}!');
   // satu line comment
@@ -7,4 +7,7 @@ void main(List<String> arguments) {
   multi line comment
   banyak line comment
     */
+    //constanta dan final
+    final String nama2 = 'Karmila';
+    print("Nama saya $nama $nama2");
 }
