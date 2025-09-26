5212aca284093c624287d4a5d06d3f2c35444854
desikaaa <desikarmila211@gmail.com>
2025-09-26 09:41:47 +0700

    Union & Intersection Set

--- project4/bin/project4.dart
+++ project4/bin/project4.dart
@@ -2,13 +2,25 @@ import 'package:project4/project4.dart' as project4;
 
 void main(List<String> arguments) {
   // Create a Growable List to store student names
-  var namaMahasiswa = <String>[];
+  // var namaMahasiswa = <String>[];
 
   // Add some names to the list
-  namaMahasiswa.add("Desi");
-  namaMahasiswa.add("Rizki");
-  namaMahasiswa.add("Siti");
+  // namaMahasiswa.add("Desi");
+  // namaMahasiswa.add("Rizki");
+  // namaMahasiswa.add("Siti");
 
   // Display the number of items in the list
-  print("Total mahasiswa ada: ${namaMahasiswa.length}");
+  // print("Total mahasiswa ada: ${namaMahasiswa.length}");
+
+  // Definisikan dua set dengan beberapa nama buah
+  var setA = <String>{'Apel', 'Jeruk', 'Mangga'};
+  var setB = <String>{'Jeruk', 'Anggur', 'Durian'};
+
+  // Hitung union dari kedua set
+  var setGabungan = setA.union(setB);
+  print("Gabungan Set A dan Set B: $setGabungan");
+
+  // Hitung intersection dari kedua set
+  var setIrisan = setA.intersection(setB);
+  print("Irisan Set A dan Set B: $setIrisan");
 }
