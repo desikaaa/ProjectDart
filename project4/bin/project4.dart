7f46bf045190f408cf8febc8a7a20e1ff70e319d
desikaaa <desikarmila211@gmail.com>
2025-09-26 09:38:01 +0700

    Growable List

--- project4/bin/project4.dart
+++ project4/bin/project4.dart
@@ -1,5 +1,14 @@
 import 'package:project4/project4.dart' as project4;
 
 void main(List<String> arguments) {
-  print('Hello world: ${project4.calculate()}!');
+  // Create a Growable List to store student names
+  var namaMahasiswa = <String>[];
+
+  // Add some names to the list
+  namaMahasiswa.add("Desi");
+  namaMahasiswa.add("Rizki");
+  namaMahasiswa.add("Siti");
+
+  // Display the number of items in the list
+  print("Total mahasiswa ada: ${namaMahasiswa.length}");
 }
