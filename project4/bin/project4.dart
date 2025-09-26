5dd73e2f8ab13027ffd9c56c4cefacff06585a28
desikaaa <desikarmila211@gmail.com>
2025-09-26 09:45:26 +0700

    union & intersection

--- project4/bin/project4.dart
+++ project4/bin/project4.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:project4/project4.dart' as project4;
 
 void main(List<String> arguments) {
@@ -12,15 +14,26 @@ void main(List<String> arguments) {
   // Display the number of items in the list
   // print("Total mahasiswa ada: ${namaMahasiswa.length}");
 
-  // Definisikan dua set dengan beberapa nama buah
-  var setA = <String>{'Apel', 'Jeruk', 'Mangga'};
-  var setB = <String>{'Jeruk', 'Anggur', 'Durian'};
+  // Fungsi untuk mendapatkan input set dari pengguna
+  Set<String> getInputSet(String setName) {
+    print("Masukkan elemen untuk $setName (pisahkan dengan koma):");
+    var input = stdin.readLineSync();
+    if (input == null || input.isEmpty) {
+      return <String>{};
+    }
+    // Pisahkan input string menjadi list dan konversi ke Set
+    return input.split(',').map((e) => e.trim()).toSet();
+  }
 
-  // Hitung union dari kedua set
-  var setGabungan = setA.union(setB);
-  print("Gabungan Set A dan Set B: $setGabungan");
+  // Dapatkan input dari pengguna untuk dua set
+  var setA = getInputSet("Set A");
+  var setB = getInputSet("Set B");
 
-  // Hitung intersection dari kedua set
-  var setIrisan = setA.intersection(setB);
-  print("Irisan Set A dan Set B: $setIrisan");
+  // Hitung dan tampilkan union (gabungan)
+  var unionSet = setA.union(setB);
+  print("\nUnion (Gabungan): $unionSet");
+
+  // Hitung dan tampilkan intersection (irisan)
+  var intersectionSet = setA.intersection(setB);
+  print("Intersection (Irisan): $intersectionSet");
 }
